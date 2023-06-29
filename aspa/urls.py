791f35b844d1a1e57393722d7043e01b07d4619e
Intern.ict <intern.ict@pwani.net>
2023-06-29 16:55:02 +0300

    created user GET view and POST view

--- aspa/urls.py
+++ aspa/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from . import views
 urlpatterns = [
   path('auth/', include('rest_auth.urls')),
+  path('all/', views.get, name='get'),
+  path('create', views.create_user, name='create-user'),
 ]
--- aspa/views.py
+++ aspa/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from aspa.models import AbstractUser,User
+from aspa.serializers import UserSerializer
+from rest_framework.exceptions import status,ValidationError
 
 # Create your views here.
+@api_view(['GET'])
+def get(request):
+    if request.query_params:
+        users = User.objects.filter(**request.query_params.dict())
+    else:
+        users = User.objects.all()
+
+    if users:
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['POST'])
+def create_user(request):
+    user =  UserSerializer(data=request.data)
+    if User.objects.filter(**request.data).exists:
+        raise ValidationError('User already exists')
+    if user.is_valid():
+        user.save()
+        return Response (user.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
